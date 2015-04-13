50499535e9b235a0e58d09be4080769bf15e72fa
erikhalmers <eriher@student.chalmers.se>
2015-04-13 11:55:04 +0200

    fix in order stub

--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ angular.module('app.services', [])
 .factory('MenuService', function(){
     
     var menuItems =
-        [{ text: 'Orders', iconClass: 'icon ion-map', link: 'menu'},
+        [{ text: 'Orders', iconClass: 'icon ion-map', link: 'orders'},
          { text: 'History',iconClass: 'icon ion-map', link: 'history'},
          { text: 'About',iconClass: 'icon ion-map', link: 'about'}];
     
