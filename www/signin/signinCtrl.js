82db7ef44273572ced766a13b4fc25b432a4fc1d
Olof <olofspetz@hotmail.com>
2015-06-02 20:24:53 +0200

    Fixed some more css

--- www/signin/signinCtrl.js
+++ www/signin/signinCtrl.js
@@ -5,24 +5,21 @@
     $scope.errror = false;
     
     $scope.signIn = function(name, password){
-        if(name.length<4 || password.length<4){
-            $scope.error = true;
-        }
-        else{
-            $scope.error = false;
+
             $ionicLoading.show({
             template: '<p class="item-icon-left">Loading... <ion-spinner icon="spiral"/></p>'
             })
-            Signin.login(name,password);  
-        }
+            Signin.login(name,password);
+        
     }
-    
+
     //Event fires when the login has failed   
     $scope.$on('event:auth-login-failed', function(e, status) {
         console.log("login failed");
         alert('Login failed!')
         $ionicLoading.hide()
-        $state.go('signin')
+        $scope.error = true;
+        //$state.go('signin')
     });
     
     //Event fires when the login is confirmed
@@ -34,7 +31,7 @@
         }
             
     });
-    
+
     //Event fires when server returns http 401 (unAuthenticated), tries to login the user again
     $scope.$on('event:auth-loginRequired', function(e, rejection) {
         var user = JSON.parse(window.localStorage['user'] || '{}');
